af9b078e569fc4609559e7a3bdbc28e8bdbba6bf
Nima Soufiani <n_soufiani@hotmail.com>
2019-11-03 23:07:12 +0900

    changes before deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 
 //Connect database
 connectDB();
@@ -8,13 +9,21 @@ connectDB();
 //init middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Lets hope this application for dad will be great" })
-);
+app.get("/", (req, res) => res.json());
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/clients", require("./routes/clients"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5001;
 
