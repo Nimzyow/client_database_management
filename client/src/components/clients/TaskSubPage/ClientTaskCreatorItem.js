34da874d210de672c094629d486f3eac7064b8c0
Nimzyow <n_soufiani@hotmail.com>
2019-11-11 00:39:13 +0900

    clienttaskcreatoritem change

--- client/src/components/clients/TaskSubPage/ClientTaskCreatorItem.js
+++ client/src/components/clients/TaskSubPage/ClientTaskCreatorItem.js
@@ -6,8 +6,45 @@ const ClientTaskCreatorItem = () => {
 
   const { current } = clientContext;
 
-  if (taskList.length === 0) {
-    return <h4>Please add a task</h4>;
+  let displayTasks = null;
+
+  if (current !== null) {
+    if (current.taskList.length === 0) {
+      return <h4>Please add a task</h4>;
+    } else if (current.taskList.length > 0) {
+      current.taskList.map(task => {
+        switch (task) {
+          case "planningpermission":
+            return (
+              displayTasks +
+              (
+                <div
+                  className="grid-2"
+                  style={{ borderBottom: "1px solid #000", marginTop: "5px" }}
+                >
+                  <div>Planning Permission</div>
+                  <div>13/09/2019</div>
+                </div>
+              )
+            );
+          case "brfpa":
+            return (
+              displayTasks +
+              (
+                <div
+                  className="grid-2"
+                  style={{ borderBottom: "1px solid #000" }}
+                >
+                  <div>Building Regulation (Full plans app)</div>
+                  <div>Date</div>
+                </div>
+              )
+            );
+          default:
+            break;
+        }
+      });
+    }
   }
 
   return (
@@ -16,17 +53,7 @@ const ClientTaskCreatorItem = () => {
         <h3 style={{ borderBottom: "1px solid #000" }}>Tasks to complete</h3>
         <h3 style={{ borderBottom: "1px solid #000" }}>Date</h3>
       </div>
-      <div
-        className="grid-2"
-        style={{ borderBottom: "1px solid #000", marginTop: "5px" }}
-      >
-        <div>Planning Permission</div>
-        <div>13/09/2019</div>
-      </div>
-      <div className="grid-2" style={{ borderBottom: "1px solid #000" }}>
-        <div>Building Regulation</div>
-        <div>Date</div>
-      </div>
+      {displayTasks}
     </Fragment>
   );
 };
